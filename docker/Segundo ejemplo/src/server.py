ed71c1da9592ec6dfae3ee4188e209efa27ae64c
Saragc44 <168918174+Saragc44@users.noreply.github.com>
2025-03-15 10:11:49 -0500

    Add files via upload

---
+++ docker/Segundo ejemplo/src/server.py
@@ -0,0 +1,14 @@
+from flask import flask
+from flask import request
+import boto3
+
+server= Flask(__name__)
+
+s3 =boto3.client('s3')
+
+@server.router("/", methods =['GET'])
+def list_buckets():
+    response= s3.list_buckets()
+    return response
+if __name__ == "__main__":
+    server.run(host='0.0.0.0')
